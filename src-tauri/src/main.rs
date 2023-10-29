ccb35dc41fec96fb0ffdac491a7f0abf68e9757f
uinstinct <61635505+uinstinct@users.noreply.github.com>
2023-10-29 19:10:15 +0530

    emit a timer to the frontend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,44 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::time::Duration;
+
 use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
+use tokio::time::{self, Instant};
+
+const TIME_LEFT_EVENT: &str = "time_left";
+
+#[tauri::command]
+async fn start_timer(window: tauri::Window) {
+    println!("starting the timer");
+    let mut interval = time::interval(Duration::from_secs(1));
+    let earlier = Instant::now();
+    loop {
+        let instant = interval.tick().await;
+        window
+            .emit(
+                &TIME_LEFT_EVENT,
+                format!("{} have elapsed", instant.duration_since(earlier).as_secs()),
+            )
+            .unwrap();
+    }
+}
 
 fn main() {
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
-    let tray_menu = SystemTrayMenu::new()
-        .add_item(quit)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(hide);
+    // let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    // let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    // let tray_menu = SystemTrayMenu::new()
+    //     .add_item(quit)
+    //     .add_native_item(SystemTrayMenuItem::Separator)
+    //     .add_item(hide);
 
-    let tray = SystemTray::new().with_menu(tray_menu);
+    // let tray = SystemTray::new().with_menu(tray_menu);
+
+    // tauri::async_runtime::set(tokio::runtime::Handle::current());
 
     tauri::Builder::default()
-        .system_tray(tray)
+        .invoke_handler(tauri::generate_handler![start_timer])
+        // .system_tray(tray)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/screens/start-session.tsx
+++ src/screens/start-session.tsx
@@ -1,11 +1,36 @@
 import { Button } from "@/components/ui/button";
 import { useI18nContext } from "@/i18n/i18n-react";
+import { UnlistenFn, listen } from "@tauri-apps/api/event";
+import { invoke } from "@tauri-apps/api/tauri";
+import { useEffect } from "react";
 
 export default function StartSession() {
   const { LL } = useI18nContext();
+
+  const handleStartTimer = () => {
+    invoke("start_timer");
+  };
+
+  useEffect(() => {
+    let unlisten: UnlistenFn | undefined;
+
+    (async () => {
+      unlisten = await listen("time_left", (event) => {
+        console.log("the event details->", event);
+      });
+    })();
+
+    return () => unlisten?.();
+  }, []);
+
   return (
     <div className="h-screen flex justify-center items-center">
-      <Button intent={"secondary"} size="large" className="rounded-lg">
+      <Button
+        intent={"secondary"}
+        size="large"
+        className="rounded-lg"
+        onClick={handleStartTimer}
+      >
         {LL.START_SESSION()}
       </Button>
     </div>
