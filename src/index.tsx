a9e7ed7f38a37b3938b0de54853478f585138c6b
uinstinct <61635505+uinstinct@users.noreply.github.com>
2024-01-08 17:12:02 +0530

    remove vite-react and install trunk-leptos

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-import "./global.css";
-import App from "./screens/main";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <>
-    <App />
-  </>
-);
